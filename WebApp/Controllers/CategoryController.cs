da5a7a8413f7713db54b88f40e7340ddbd675009
arohunaniiann@mail.ru <arohunaniiann@mail.ru>
2022-10-23 22:24:43 +0400

    Add project files.

---
+++ WebApp/Controllers/CategoryController.cs
@@ -0,0 +1,117 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApp.Data;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+    
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objCategoryList = _db.Categories;
+            return View(objCategoryList);
+        }
+        // GET: CategoriesTemp/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _db.Categories == null)
+            {
+                return NotFound();
+            }
+
+            var category = await _db.Categories
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return View(category);
+        }
+
+        //Get
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category category)
+        {
+         
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(category);
+                _db.SaveChanges();
+                TempData["success"] = "New Address Created Successfully";
+                return RedirectToAction("Index");
+            }
+            return View(category);
+            
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if(id ==null || id == 0)
+            {
+                return NotFound();
+            }
+             var objCategory = _db.Categories.Find(id);
+            if(objCategory == null)
+            {
+                return NotFound();
+            }
+            return View(objCategory);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category category)
+        {
+
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(category);
+                _db.SaveChanges();
+                TempData["success"] = "Address Edited Successfully";
+                return RedirectToAction("Index");
+            }
+            return View(category);
+
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var objCategory = _db.Categories.Find(id);
+            if (objCategory == null)
+            {
+                return NotFound();
+            }
+            return View(objCategory);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(Category category)
+        {
+
+           
+                _db.Categories.Remove(category);
+                _db.SaveChanges();
+                TempData["success"] = "Address Deleted Successfully";
+            return RedirectToAction("Index");
+            
+          
+
+        }
+    }
+}
---
+++ WebApp/Models/Category.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApp.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string FullName { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public int PhoneNumber { get; set; }
+        
+        public string? PhysicalAddress { get; set; }
+    }
+}
